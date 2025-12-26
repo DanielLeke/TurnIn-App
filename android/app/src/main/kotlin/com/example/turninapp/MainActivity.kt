6b5f932e9734dcacca288e1e0a736a7787a6adc3
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-27 00:22:32 +0100

    Initial commit. Started building the ui for the students page

---
+++ android/app/src/main/kotlin/com/example/turninapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.turninapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/students/students_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class StudentsPage extends StatelessWidget {
+  const StudentsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: studentsAppBar(),
+    );
+  }
+
+  AppBar studentsAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      title: Text('My Submissions'),
+      leading: Icon(Icons.insert_drive_file, color: Colors.blue,),
+      actions: [
+        IconButton(onPressed: () {}, icon: Icon(Icons.logout, color: Colors.grey[100],))
+      ],
+    );
+  }
+}
+

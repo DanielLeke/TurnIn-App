86433f601af4dfbf297acdf2adc3cc4fdc2eb3b5
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-27 00:39:32 +0100

    Created a button to allow users submit submissions (Not functional yet)

--- lib/students/students_page.dart
+++ lib/students/students_page.dart
@@ -8,7 +8,7 @@ class StudentsPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: studentsAppBar(),
-      body: ListView(),
+      body: ListView(children: [CreateSubmissionBtn()]),
     );
   }
 
@@ -16,11 +16,37 @@ class StudentsPage extends StatelessWidget {
     return AppBar(
       backgroundColor: Colors.white,
       title: Text('My Submissions'),
-      leading: Icon(Icons.insert_drive_file, color: Colors.blue,),
+      leading: Icon(Icons.insert_drive_file, color: Colors.blue),
       actions: [
-        IconButton(onPressed: () {}, icon: Icon(Icons.logout, color: Colors.grey[100],))
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.logout, color: Colors.grey[100]),
+        ),
       ],
     );
   }
 }
 
+class CreateSubmissionBtn extends StatelessWidget {
+  const CreateSubmissionBtn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+    onPressed: () {}, 
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Colors.blue,
+      foregroundColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5)
+      )
+    ),
+    child: Text(
+      "+ New Submission",
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold
+      ),
+    ));
+  }
+}

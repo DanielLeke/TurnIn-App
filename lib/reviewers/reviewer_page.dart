145d97f47f7816372f5473e529e7248221108e19
Daniel Adeleke <danieladeleke05@gmail.com>
2025-12-31 21:06:12 +0100

    Created a page for the reviewer ui

---
+++ lib/reviewers/reviewer_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReviewerPage extends StatelessWidget {
+  const ReviewerPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
